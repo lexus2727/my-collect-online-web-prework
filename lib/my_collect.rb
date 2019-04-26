2258ece6be475903478a2049c354b464ac99fe3f
compassionate-scope-9333 <lexus2424@gmail.com>
2019-04-26 13:12:11 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+collection = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller'] 
+
+my_collect(collection)
+
+ i = 0
+new_array = []
+
+while i < collection.length
+     new_array << yield
+       collection[i]
+         i += 1
+         end 
+       new_array
+
+end
+ # my_collect(collection) {|i| i.split(" ").first}
 
 
